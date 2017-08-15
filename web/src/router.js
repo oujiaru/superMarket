3282816e67664324a1edb6c773117027a2eb9886
Gpxccc <gpxaaa@163.com>
2017-08-15 15:06:57 +0800

    jingtai

--- web/src/router.js
+++ web/src/router.js
@@ -3,10 +3,16 @@ import {Route} from 'react-router'
 
 import AppComponent from './modules/app/AppComponent'
 import LoginComponent from './modules/login/LoginComponent'
+import POComponent from './modules/PO/POComponent'
+import QuoteComponent from './modules/Quote/QuoteComponent'
+import QcompileComponent from './modules/Qcompile/QcompileComponent'
 
 export default [
 	<Route path="/" component={AppComponent}>
 		
 	</Route>,
-	<Route path="/login" component={LoginComponent}/>
+	<Route path="/login" component={LoginComponent}/>,
+	<Route path="/PO" component={POComponent} />,
+	<Route path="/Quote" component={QuoteComponent} />,
+	<Route path="/Qcompile" component={QcompileComponent} />
 ]
